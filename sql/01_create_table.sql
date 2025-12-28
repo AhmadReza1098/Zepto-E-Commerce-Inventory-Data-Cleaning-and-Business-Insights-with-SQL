c4cd1cd8db1360b6ce4bd281ad1d038c9cf01d2d
Ahmad Reza <ahmadreza6122@gmail.com>
2025-12-28 21:27:53 +0530

    Update 01_create_table.sql

--- sql/01_create_table.sql
+++ sql/01_create_table.sql
@@ -1,1 +1,16 @@
+DROP TABLE IF EXISTS zepto_inventory;
 
+use zepto;
+
+-- Create table in database
+CREATE TABLE zepto_inventory (
+    category VARCHAR(120),
+    name VARCHAR(150) NOT NULL,
+    mrp DECIMAL(10 , 2 ),
+    discountPercent DECIMAL(5 , 2 ),
+    availableQuantity INT,
+    discountedSellingPrice DECIMAL(10 , 2 ),
+    weightInGms INT,
+    outOfStock VARCHAR(50),
+    quantity INT
+);
